370c3fcab418ccb63cad6647258d303f43954070
Arthur Liu <lyz9518@163.com>
2022-04-17 23:51:43 -0400

    add new regulatory data

--- tutorial_data.py
+++ tutorial_data.py
@@ -1,11 +1,147 @@
 tutorial = {
     "regulatory":{
-        1:{
+        1: {
             "id": 1,
             "name": "STOP SIGN",
             "image_link": "XXX",
             "hint": "Look at the text inside the sign",
             "description": "The STOP sign is the only octagon-shaped (8-sided) sign you see on the highway. At an intersection with a STOP sign, you must stop completely, check for pedestrians and cross traffic to clear the intersection before you go again. Slowing down without coming to a full stop is illegal.",
+            "category": "Regulatory Sign"
+        },
+        2:{
+            "id": 2,
+            "name": "YIELD SIGN",
+            "image_link": "",
+            "hint": "Look at the text inside the sign",
+            "description": "YIELD signs are triangular (3-sided) in shape. When you see this sign, you must slow down and check for traffic and give the right-of-way to pedestrians and approaching cross traffic. You stop only when it is necessary. Proceed when you can do so safely without interfering with normal traffic flow. Remember, you must have a sufficient gap in traffic before you can continue on at either STOP or YIELD sign locations.",
+            "category": "Regulatory Sign"
+        },
+        3:{
+            "id": 3,
+            "name": "ONE WAY SIGN",
+            "image_link": "",
+            "hint": "Arrow = Road Direction, Text, Black=Allow",
+            "description": "The ONE-WAY sign means traffic is allowed to move only in the direction the sign is pointing. If you turn onto a one-way street traveling in the opposite direction, you are likely to get into a head-on collision.",
+            "category": "Regulatory Sign"
+        },
+        4:{
+            "id": 4,
+            "name": "RAILROAD CROSSBUCK SIGN",
+            "image_link": "",
+            "hint": "Text, Crossing shape",
+            "description": "RAILROAD CROSSBUCK SIGN This sign is placed at a railroad crossing where the tracks cross the roadway. You should treat the crossbuck sign as a YIELD sign; slow down and prepare to stop, if you see or hear a train approaching. Refer to Chapter 3 for more information about railroad crossing safety.",
+            "category": "Regulatory Sign"
+        },
+        5:{
+            "id": 5,
+            "name": "DO NOT ENTER SIGN",
+            "image_link": "",
+            "hint": "Text, Red=Not Allowed",
+            "description": "The DO NOT ENTER and WRONG WAY signs work as a team. The DO NOT ENTER sign is put at the beginning of one-way streets and ramps. When you see this sign, do not drive onto that street or ramp in the direction you are heading.",
+            "category": "Regulatory Sign"
+        },
+        6:{
+            "id": 6,
+            "name": "DIVIDED HIGHWAY SIGN",
+            "image_link": "",
+            "hint": "Arrow = Road Direction, Text",
+            "description": "This sign means the road you are on intersects with a divided highway. The divided highway is really two, one-way roadways that are separated by a median or a guide rail. At the first roadway, you can only turn right, and at the second roadway, you can only turn left.",
+            "category": "Regulatory Sign"
+        },
+        7:{
+            "id": 7,
+            "name": "NO TRUCKS SIGN",
+            "image_link": "",
+            "hint": "Transportation Tool, Red=NO",
+            "description": "Operators of trucks may not use roadways where these signs are posted.",
+            "category": "Regulatory Sign"
+        },
+        8:{
+            "id": 8,
+            "name": "NO U-TURN SIGN",
+            "image_link": "",
+            "hint": "Red=No, Returned Arrow=U Turn",
+            "description": "This sign prohibits U-turns. Do not make a complete turn to go in the opposite direction.",
+            "category": "Regulatory Sign"
+        },
+        9:{
+            "id": 9,
+            "name": "NO PARKING",
+            "image_link": "",
+            "hint": "P=Parking, Red=No",
+            "description": "You may not park a vehicle at locations where this sign is posted. Unless there is a allowed time limit notation",
+            "category": "Regulatory Sign"
+        },
+        10:{
+            "id": 10,
+            "name": "CENTER LANE LEFT TURN ONLY SIGN",
+            "image_link": "",
+            "hint": "Arrow = Road Direction, Drive along the sign direction only",
+            "description": "This sign indicates where a lane is reserved for the exclusive use of left-turning vehicles in either direction and is not to be used for through traffic or passing.",
+            "category": "Regulatory Sign"
+        },
+        11:{
+            "id": 11,
+            "name": "TURN TO LEFT OR RIGHT SIGN",
+            "image_link": "",
+            "hint": "Black=Allowed, Follow the arrows",
+            "description": "If you are in a lane controlled by signs like these, you may travel in either direction the arrows point.",
+            "category": "Regulatory Sign"
+        },
+        12:{
+            "id": 12,
+            "name": "KEEP RIGHT SIGN",
+            "image_link": "",
+            "hint": "Text, Arrow direction",
+            "description": "Keep to the right of the traffic island or divider",
+            "category": "Regulatory Sign"
+        },
+        13:{
+            "id": 13,
+            "name": "RIGHT TURN SIGNAL",
+            "image_link": "",
+            "hint": "Text",
+            "description": "This sign is posted close to a traffic signal to indicate that the signal controls right-turn movements.",
+            "category": "Regulatory Sign"
+        },
+        14:{
+            "id": 14,
+            "name": "SPEED LIMIT SIGN",
+            "image_link": "",
+            "hint": "Text, Number",
+            "description": "A SPEED LIMIT sign indicates the maximum legal speed under ideal conditions for the stretch of highway where it is posted. Your safety may depend upon driving slower than the posted speed under poor conditions.",
+            "category": "Regulatory Sign"
+        },
+        15:{
+            "id": 15,
+            "name": "NO TURN ON RED SIGN",
+            "image_link": "",
+            "hint": "Text, Be careful black doesn't mean allow here",
+            "description": "You may not turn during the red light cycle at an intersection where one of these signs is posted. You must wait for the signal to turn green.",
+            "category": "Regulatory Sign"
+        },
+        16:{
+            "id": 16,
+            "name": "LEFT TURN YIELD ON GREEN SIGN",
+            "image_link": "",
+            "hint": "Text, Pay attention to the YIELD word",
+            "description": "This sign indicates a left turn is permitted, but you must yield to oncoming traffic. It is commonly placed at intersections, near the left-turn traffic signal, to remind drivers left turns are not protected when the circular green signal comes on.",
+            "category": "Regulatory Sign"
+        },
+        17:{
+            "id": 17,
+            "name": "OPPOSING TRAFFIC HAS EXTENDED GREEN SIGN",
+            "image_link": "",
+            "hint": "Text, needs to be read carefully",
+            "description": "This sign is posted where opposing traffic may continue to move after your signal has turned red.",
+            "category": "Regulatory Sign"
+        },
+        18:{
+            "id": 18,
+            "name": "RESERVED PARKING",
+            "image_link": "",
+            "hint": "If you are not disabled, do not park here!!!",
+            "description": " Although this sign is not black and white, it is a regulatory sign. It marks areas where parking is reserved for disabled persons or severely disabled veterans. Vehicles parked in these spaces must display authorized registration plates or parking placards. Unauthorized vehicles parked in these spaces are subject to a fine and towing costs.",
             "category": "Regulatory Sign"
         }
     },
@@ -20,3 +156,5 @@ tutorial = {
         }
     }
 }
+
+
