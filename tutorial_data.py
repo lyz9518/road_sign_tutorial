f89d599c33cf4afcea0e63fd1fdb5c82be66369f
Arthur Liu <lyz9518@163.com>
2022-04-18 00:56:54 -0400

    Warning data added

--- tutorial_data.py
+++ tutorial_data.py
@@ -146,12 +146,204 @@ tutorial = {
         }
     },
     "warning":{
-        19:{
-            "id": 19,
-            "name": "LEFT CURVE",
-            "image_link": "XXX",
-            "hint": "Yellow background, Arrow = Road",
+        21:{
+            "id": 21,
+            "name": "NO PASSING ZONE SIGN",
+            "image_link": "",
+            "hint": "Yellow=Warning, Text",
+            "description": "This is the only pennant-shaped warning sign. This sign marks the beginning of a no-passing zone and is placed on the left side of the road facing the driver.",
+            "category": "Warning Sign"
+        },
+        22:{
+            "id": 22,
+            "name": "CHEVRON SIGNS",
+            "image_link": "",
+            "hint": "Yellow=Warning, Arrow=Direction, Usually appear in multiple",
+            "description": "There is a sharp change in the direction of the road, such as a curve to the left or right. The road bends in the direction the chevron points. When used in a curve, there will be an advanced curve warning sign, and there may be several chevron signs placed throughout the curve.",
+            "category": "Warning Sign"
+        },
+        23:{
+            "id": 23,
+            "name": "LEFT CURVE SIGN",
+            "image_link": "",
+            "hint": "Yellow=Warning, Arrow=Direction",
             "description": "The road curves ahead to the left. You need to slow down, stay in the center of your lane, and prepare to navigate through the left-bending curve.",
+            "category": "Warning Sign"
+        },
+        24:{
+            "id": 24,
+            "name": "SHARP LEFT TURN SIGN",
+            "image_link": "",
+            "hint": "Yellow=Warning, Arrow=Direction, Rectangular=Sharp Angle",
+            "description": "The road ahead turns sharply to the left. You need to slow down substantially, stay in the center of your lane and prepare to navigate through the sharp left turn. Some sharp turn signs have an advisory speed located on the sign or posted below it.",
+            "category": "Warning Sign"
+        },
+        25:{
+            "id": 25,
+            "name": "SET OF CURVES SIGN",
+            "image_link": "",
+            "hint": "Smooth arrow=curve",
+            "description": "The road ahead curves first in one direction and then back in the other direction (a set of two curves). Slow down and prepare to navigate through the set of curves.",
+            "category": "Warning Sign"
+        },
+        26:{
+            "id": 26,
+            "name": "",
+            "image_link": "",
+            "hint": "",
+            "description": "",
+            "category": "Warning Sign"
+        },
+        27:{
+            "id": 27,
+            "name": "RIGHT AND LEFT SHARP TURNS SIGN",
+            "image_link": "",
+            "hint": "Sharp arrow=Sharp turn",
+            "description": "The road ahead turns sharply first in one direction and then back in the other direction. Slow down substantially and prepare to navigate through the set of turns.",
+            "category": "Warning Sign"
+        },
+        28:{
+            "id": 28,
+            "name": "WINDING ROAD SIGN",
+            "image_link": "",
+            "hint": "Smooth zigzag = Windy",
+            "description": "This sign indicates there are three or more curves in a row on the road ahead.",
+            "category": "Warning Sign"
+        },
+        29:{
+            "id": 29,
+            "name": "ONE DIRECTION LARGE ARROW SIGN",
+            "image_link": "",
+            "hint": "Straight arrow=sudden, one direction",
+            "description": "The road ahead changes direction at an extreme angle. Before you reach such an extreme curve, slow down as much as you would to make a turn at an intersection.",
+            "category": "Warning Sign"
+        },
+        30:{
+            "id": 30,
+            "name": "ROAD ENTERING CURVE SIGN",
+            "image_link": "",
+            "hint": "Arrow = Direction,  Little line = merging road",
+            "description": "The main road curves to the left with a side road entering from the right. Approach the intersection with extra caution. A driver preparing to enter the main road may not be able to see you approaching from around the curve and may pull out in front of you, leaving you little room to avoid a crash, if you are traveling too fast.",
+            "category": "Warning Sign"
+        },
+        31:{
+            "id": 31,
+            "name": "TRUCK ROLLOVER SIGN",
+            "image_link": "",
+            "hint": "Truck, fall down on one side",
+            "description": "This sign identifies curves where trucks traveling at excessive speeds have a potential to rollover.",
+            "category": "Warning Sign"
+        },
+        32:{
+            "id": 32,
+            "name": "MERGING TRAFFIC SIGN",
+            "image_link": "",
+            "hint": "Two to one=merger",
+            "description": " This sign is placed on a major roadway to tell you traffic may be merging into your lane from another roadway or a ramp. In this example, the traffic will be entering your lane from the right. Entering traffic must yield to traffic already on the major roadway.",
+            "category": "Warning Sign"
+        },
+        33:{
+            "id": 33,
+            "name": "ADDED LANE SIGN",
+            "image_link": "",
+            "hint": "you will see the other car very soon",
+            "description": "This sign is placed in advance of a point where two roadways meet. It means merging is not necessary because the entering traffic uses a new lane that has been added to the roadway.",
+            "category": "Warning Sign"
+        },
+        34:{
+            "id": 34,
+            "name": "WEAVE AREA SIGN",
+            "image_link": "",
+            "hint": "shape",
+            "description": "This sign is used to warn that you will be merging with another roadway as some traffic is exiting and crossing your path.",
+            "category": "Warning Sign"
+        },
+        35:{
+            "id": 35,
+            "name": "DIVIDED HIGHWAY BEGINS SIGN",
+            "image_link": "",
+            "hint": "Will see a stone=divided highway starts",
+            "description": "The highway ahead becomes a divided highway with two one-way roadways and a median or divider separating traffic moving in opposite directions. Keep to the right of the divider or median.",
+            "category": "Warning Sign"
+        },
+        36:{
+            "id": 36,
+            "name": "TWO-WAY TRAFFIC AHEAD SIGN",
+            "image_link": "",
+            "hint": "You can go either way",
+            "description": "The one-way street or roadway ends ahead. You will then be traveling on a road with two lanes of traffic moving in opposite directions. Stay in your lane to the right of the centerline. This sign may also be placed at intervals along the two-way roadway.",
+            "category": "Warning Sign"
+        },
+        37:{
+            "id": 37,
+            "name": "TWO DIRECTION LARGE ARROW SIGN",
+            "image_link": "",
+            "hint": "No other arrow=either left or right",
+            "description": "This sign is placed at the far side of a T-intersection to tell you that you can only go left or right. The road you are on does not continue straight through at the intersection.",
+            "category": "Warning Sign"
+        },
+        38:{
+            "id": 38,
+            "name": "NARROW BRIDGE OR UNDERPASS AHEAD SIGN",
+            "image_link": "",
+            "hint": "Narrow in the middle=Narrow bridge",
+            "description": "Each lane on the bridge or underpass becomes narrower, and there generally is no shoulder. You need to slow down and make sure you stay in your lane. If a driver approaching from the opposite direction has a wide vehicle or is taking up part of your lane, wait until they clear the area.",
+            "category": "Warning Sign"
+        },
+        39:{
+            "id": 39,
+            "name": "SLIPPERY WHEN WET SIGN",
+            "image_link": "",
+            "hint": "Car can't go straight=slippery",
+            "description": "Especially slippery conditions exist when wet. In wet weather, reduce your speed and increase your following distance. Do not accelerate or brake quickly. Make turns at a very slow speed.",
+            "category": "Warning Sign"
+        },
+        40:{
+            "id": 40,
+            "name": "DEER CROSSING SIGN",
+            "image_link": "",
+            "hint": "Deer jumping to the road",
+            "description": "This sign is posted in areas where deer frequently cross and crashes with deer have occurred.",
+            "category": "Warning Sign"
+        },
+        41:{
+            "id": 41,
+            "name": "RAILROAD CROSSING AHEAD",
+            "image_link": "",
+            "hint": "R=Railroad, X=crossing",
+            "description": "You are approaching a railroad crossing. When you see this sign, begin looking and listening for a train. This sign is placed far enough in advance to allow you to stop before you reach the tracks, if necessary. Refer to Chapter 3 for more information about railroad crossing safety.",
+            "category": "Warning Sign"
+        },
+        42:{
+            "id": 42,
+            "name": "ADVANCED INTERSECTION/RAILROAD CROSSING SIGN",
+            "image_link": "",
+            "hint": "Complicated image=Advanced, Road&Rail=crossing",
+            "description": " The intersecting road has a railroad crossing within a very short distance from the intersection. If you are planning to turn onto the intersecting road and if a train is approaching, be prepared to stop.",
+            "category": "Warning Sign"
+        },
+        43:{
+            "id": 43,
+            "name": "BICYCLE/MOTOR VEHICLE SHARE THE ROAD SIGN",
+            "image_link": "",
+            "hint": "Bike & Car = Share the road",
+            "description": "Used at roadway pinch points (no shoulder, narrow lanes, etc.) to warn you, as a motorist, to provide adequate space for bicyclists to share the roadway.",
+            "category": "Warning Sign"
+        },
+        44:{
+            "id": 44,
+            "name": "SCHOOL SIGN",
+            "image_link": "",
+            "hint": "Adult & Children=teacher and student",
+            "description": "This is the only pentagon-shaped sign used on the roadway. It means you are approaching a school zone. If the sign is accompanied by a downward-pointing arrow, it means you are at the school zone crosswalk. You should slow down, watch for children crossing the road and stop, if necessary. This sign may have either a yellow background (as shown) or a fluorescent yellow-green background.",
+            "category": "Warning Sign"
+        },
+        45:{
+            "id": 45,
+            "name": "HORSE-DRAWN VEHICLE",
+            "image_link": "",
+            "hint": "Wagon=Horse can ride too",
+            "description": "This sign is posted in areas where slow-moving, horse-drawn vehicles cross or share the roadway with motorized vehicles.",
             "category": "Warning Sign"
         }
     }
